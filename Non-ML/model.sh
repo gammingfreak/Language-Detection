6909bdab9dd1fa5af547f2875b49ea070646938a
Yifang Wang <wangyifang@outlook.com>
2018-12-16 11:33:08 +0000

    Delete model.sh

--- Non-ML/model.sh
+++
@@ -1,6 +0,0 @@
-./kenlm/build/bin/build_binary ita.arpa ita.klm
-./kenlm/build/bin/build_binary fra.arpa fra.klm
-./kenlm/build/bin/build_binary eng.arpa eng.klm
-./kenlm/build/bin/build_binary por.arpa por.klm
-./kenlm/build/bin/build_binary deu.arpa deu.klm
-./kenlm/build/bin/build_binary spa.arpa spa.klm
